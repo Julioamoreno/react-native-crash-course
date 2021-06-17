eca22b12338533e0f858b783223239528c7993e3
Julio <julioarmando321@gmail.com>
2021-06-16 22:01:46 -0300

    feat: add request to api and getting weather data

--- App.js
+++ App.js
@@ -1,21 +1,76 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [errorMsg, setErrorMsg] = useState(null);
+	const [latitude, setLatitude] = useState(null);
+	const [longitude, setLongitude] = useState(null);
+	const [currentWeather, setCurrentWeather] = useState(null);
+	const [unitsSystem, setUnitsSystem] = useState('metric');
+
+	useEffect(() => {
+		(async () => {
+			try {
+				let { status } = await Location.requestForegroundPermissionsAsync();
+				if (status !== 'granted') {
+					setErrorMsg('Permission to access location was denied');
+					return;
+				}
+
+				let location = await Location.getCurrentPositionAsync({});
+				setLatitude(location.coords.latitude);
+				setLongitude(location.coords.longitude);
+			} catch (err) {
+				setErrorMsg(err.message);
+			}
+		})();
+	}, []);
+
+	useEffect(() => {
+		if (!latitude || !longitude) return;
+		(async () => {
+			weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=${unitsSystem}&appid=${process.env.WEATHER_API_KEY}`;
+			const response = await fetch(weatherUrl);
+
+			const result = await response.json();
+			if (response.status === 200) {
+				setCurrentWeather(result);
+			} else {
+				setErrorMsg(result.message);
+			}
+		})();
+	}, [latitude, longitude]);
+
+	if (!!currentWeather) {
+		const {
+			main: { temp },
+		} = currentWeather;
+		return (
+			<View style={styles.container}>
+				{errorMsg && <Text>{`Error: ${errorMsg}`}</Text>}
+				<Text>{`${temp}º`}</Text>
+				<Text>Open up App.js to start working on your app!</Text>
+				<StatusBar style='auto' />
+			</View>
+		);
+	} else {
+		return (
+			<View style={styles.container}>
+				{errorMsg && <Text>{`Error: ${errorMsg}`}</Text>}
+				<Text>Open up App.js to start working on your app!</Text>
+				<StatusBar style='auto' />
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
