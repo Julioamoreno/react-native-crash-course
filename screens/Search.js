5d8373be0bc1557bf65e688b0469b56c3881bfca
Julio <julioarmando321@gmail.com>
2021-06-22 10:16:14 -0300

    style: changing background color

--- screens/Search.js
+++ screens/Search.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
 import FormSubmitCity from '../components/FormSubmitCity';
 import PreviousSearches from '../components/PreviousSearches';
 
 export default function SearchScreen({ navigation }) {
 	return (
-		<View>
+		<View style={styles.main}>
 			<FormSubmitCity navigation={navigation} />
 			<PreviousSearches navigation={navigation} />
 		</View>
 	);
 }
+
+const styles = StyleSheet.create({
+	main: {
+		backgroundColor: 'white',
+		flex: 1,
+	},
+});
